33787429b33a0a14ac2787b3c2cf75587f2fee92
kiermcguirk <kier.mcguirk@ucdconnect.ie>
2022-06-22 16:52:05 +0100

    Info section

---
+++ my-website/src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'About Me',
+    headline: 'UCD Computer Science with Data Science 18/22',
+    description: 'Recent UCD Computer Science Graduate looking for work in the Software Engineering space, with passion for full-stack development. ',
+    buttonLabel: 'Contact',
+    imgStart: false,
+    img: require('../../images/profile-pic.png'),
+    alt: 'Profile Picture',
+    dark: true,
+    primary: true,
+    darkText: false
+
+}
+
+export const homeObjTwo = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Premium Bank',
+    headline: 'unlimited transactions with 0 fees',
+    description: 'Get Access to our exclusive app that allows you to send unlimitted transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/react-logo.png'),
+    alt: 'Car',
+    dark: false,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Premium Bank',
+    headline: 'unlimited transactions with 0 fees',
+    description: 'Get Access to our exclusive app that allows you to send unlimitted transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: true
+
+}
