7aca7b9ff2394c00e8aed167df56462c0fc20694
kiermcguirk <kier.mcguirk@ucdconnect.ie>
2022-06-22 16:51:47 +0100

    hero section

---
+++ my-website/src/components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import Video from '../../videos/matrix.mp4';
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper,ArrowForward, ArrowRight} from './HeroSectionElements';
+import { Button } from '../ButtonElements';
+import NameTag from '../NameTag';
+import DevelopmentCard from '../DevelopmentCard';
+const HeroSection = () => {
+
+  const [hover, setHover] = useState(false);
+  const onHover = () =>{
+      setHover(!hover);
+  }
+  return (
+    <HeroContainer >
+        <HeroBg>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4'></VideoBg>
+        </HeroBg>
+        <HeroContent>
+            <HeroH1>
+                {/* Kier McGuirk Project Showcase */}
+                <NameTag/>
+            </HeroH1>
+            <HeroP>
+                Showcasing projects in both the Software Engineering and Data Science space
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
+                primary='true'
+                dark='true'
+                >
+                    View Showcase {hover ? <ArrowForward/> : <ArrowRight/>}
+                </Button>
+            </HeroBtnWrapper>
+            <DevelopmentCard/>
+        </HeroContent>
+        
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
