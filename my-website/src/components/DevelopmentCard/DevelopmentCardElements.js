1e0803a6aedb55379d5c84efb1b9a32161ce90b4
kiermcguirk <kier.mcguirk@ucdconnect.ie>
2022-06-22 16:50:52 +0100

    Development card elements

---
+++ my-website/src/components/DevelopmentCard/DevelopmentCardElements.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const DevelopmentCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-color: black;
+    /* padding: 10px 10px; */
+    background-color: none;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    /* margin-top: 30px; */
+`
+
+export const DevelopmentCardParagraph = styled.p`
+`
+
+export const DevelopmentCardButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: none;
+`
+
